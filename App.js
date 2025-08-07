0dc6f2ee283ae977466b72a583173f36a36a1901
queenmalik963 <queenmalik963@gmail.com>
2025-08-08 02:35:09 +0400

    Update App.js

--- App.js
+++ App.js
@@ -20,3 +20,27 @@ export default function App() {
     </NavigationContainer>
   );
 }
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./LoginScreen";
+import HomeScreen from "./HomeScreen";
+import ProfileScreen from "./ProfileScreen";
+import VideoPlayer from "./VideoPlayer";
+import ChatBox from "./ChatBox";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Video" component={VideoPlayer} />
+        <Stack.Screen name="Chat" component={ChatBox} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+    }
