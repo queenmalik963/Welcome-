04159206c31da3c8389337c14495501dbffa8ee5
queenmalik963 <queenmalik963@gmail.com>
2025-08-08 02:18:29 +0400

    Create ChatBox.js

---
+++ ChatBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function ChatBox() {
+  return (
+    <View>
+      <Text>Chat Box Component</Text>
+    </View>
+  );
+}
