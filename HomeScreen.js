0d3b2fe27a13df7961a9edcd36b50b54f27d0790
queenmalik963 <queenmalik963@gmail.com>
2025-08-08 02:13:59 +0400

    Create HomeScreen.js

---
+++ HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Welcome to Rave Clone</Text>
+      <Button title="Join Room" onPress={() => navigation.navigate('Room')} />
+      <Button title="Profile" onPress={() => navigation.navigate('Profile')} />
+    </View>
+  );
+}
