11b77c83c6bf26a558255d23ea37f2a0aa39a207
queenmalik963 <queenmalik963@gmail.com>
2025-08-08 02:17:48 +0400

    Create VideoPlayer.js

---
+++ VideoPlayer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function VideoPlayer() {
+  return (
+    <View>
+      <Text>Video Player Component</Text>
+    </View>
+  );
+}
